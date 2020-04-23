98168cef33cc18cab64bfbf3e27016849c23b35a
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-04-23 09:57:49 +0200

    Add deployment

--- packages/app/src/views/Home/index.jsx
+++ packages/app/src/views/Home/index.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <>
       <Head>
-        <title>McKinsey Demo App</title>
+        <title>McKinsey Demo</title>
         <meta name="description" content="Demo app for McKinsey" />
       </Head>
       <main data-testid="Home_Main">Hello, world!</main>
