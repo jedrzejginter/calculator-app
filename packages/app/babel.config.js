f9493831615478574cf7b79e48e902ba94fea54c
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-04-24 13:30:23 +0200

    Try add pre-commit hooks

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -2,8 +2,8 @@ module.exports = (api) => {
   const presets = ["next/babel", "@emotion/babel-preset-css-prop"];
   const plugins = ["emotion"];
 
-  // It's important to not remove data-testid's in CI environment,
-  // because tests will fail.
+  // It's important to keep data-testid's in CI environment,
+  // because tests will fail if they're removed.
   if (api.env("production") && !api.env("test")) {
     plugins.unshift("babel-plugin-jsx-remove-data-test-id");
   }
